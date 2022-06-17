f345a9a10c66ebaeeb48577b1b704d747c44ee62
edna <chrissx@chrissx.de>
2022-06-17 16:45:10 +0200

    dependency upgrade
    nyxx is just getting worse and worse...

--- bin/jana.dart
+++ bin/jana.dart
@@ -2,31 +2,34 @@ import 'package:nyxx/nyxx.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 void main(List<String> argv) {
-  final bot = Nyxx(argv.first, GatewayIntents.allUnprivileged,
+  final bot = NyxxFactory.createNyxxWebsocket(
+      argv.first, GatewayIntents.allUnprivileged,
       options: ClientOptions(guildSubscriptions: false));
   final yt = YoutubeExplode();
 
-  bot.onMessageReceived.listen((event) async {
+  bot.eventsWs.onMessageReceived.listen((event) async {
     final msg = event.message;
     final channel = await msg.channel.getOrDownload();
     print('Msg from ${str(msg.author)}: ${msg.content} (${msg.url})');
     if (event.message.content == '!ping') {
-      await channel.sendMessage(content: 'Pong!');
+      await channel.sendMessage(MessageBuilder.content('Pong!'));
     }
   });
 
   checkYoutube(bot, yt, []);
 }
 
-void checkYoutube(Nyxx bot, YoutubeExplode yt, List<String> sent) async {
+void checkYoutube(
+    INyxxWebsocket bot, YoutubeExplode yt, List<String> sent) async {
   final streams =
       yt.channels.getUploads('UCZs3FO5nPvK9VveqJLIvv_w').where((v) => v.isLive);
   if (!(await streams.isEmpty)) {
     final stream = (await streams.first).id.value;
     if (!sent.contains(stream)) {
       final channel =
-          await bot.fetchChannel<TextChannel>(Snowflake('826983242493591592'));
-      await channel.sendMessage(content: '@everyone https://youtu.be/$stream');
+          await bot.fetchChannel<ITextChannel>(Snowflake('826983242493591592'));
+      await channel.sendMessage(
+          MessageBuilder.content('@everyone https://youtu.be/$stream'));
       sent.add(stream);
     }
   }
