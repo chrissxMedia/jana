62a03fa26dea1cea2be4488feecb617ffdb6ca0e
pixel christa edna <chrissx@chrissx.de>
2022-07-09 02:56:40 +0200

    1.0.3: new video narrowing algorithm
    no more caching required ;)
    
    (might have to reintroduce it later though)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.3
+
+- New video narrowing algorithm with many advantages
+
 ## 1.0.2
 
 - News are now posted into the proper channel
--- bin/jana.dart
+++ bin/jana.dart
@@ -1,50 +1,18 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:nyxx/nyxx.dart';
-import 'package:stash/stash_api.dart';
-import 'package:stash_file/stash_file.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 void log(Object? msg) => print('[${DateTime.now()}] $msg');
 
 final internal = Snowflake('826983242493591592');
 final news = Snowflake('551908144641605642');
-late final Cache<Map> cache;
 final yt = YoutubeExplode();
 
-Future<Map> getVideo(String id) async {
-  final cached = await cache.get(id);
-  if (cached != null) return cached;
-  log('Cache miss for $id');
-  final r = await yt.videos.get(id);
-  final vid = {
-    'url': r.url,
-    'author': r.author,
-    'channelId': r.channelId.value,
-    'description': r.description,
-    'duration': r.duration?.toString(),
-    'likes': r.engagement.likeCount,
-    'dislikes': r.engagement.dislikeCount,
-    'rating': r.engagement.avgRating,
-    'id': r.id.value,
-    'isLive': r.isLive,
-    'keywords': r.keywords,
-    'publishDate': r.publishDate?.toIso8601String(),
-    'thumbnailMax': r.thumbnails.maxResUrl,
-    'title': r.title,
-    'uploadDate': r.uploadDate?.toIso8601String(),
-  };
-  await cache.put(id, vid);
-  return vid;
-}
+Stream<String> getVideoIds() =>
+    yt.channels.getUploads('UCZs3FO5nPvK9VveqJLIvv_w').map((v) => v.id.value);
 
 void main(List<String> argv) async {
-  cache = await Directory('/var/cache')
-      .create()
-      .then((_) => newFileLocalCacheStore(path: '/var/cache/jana'))
-      .then((store) => store.cache<Map>(name: 'yt_vids'));
-
   final bot = NyxxFactory.createNyxxWebsocket(
       argv.first, GatewayIntents.allUnprivileged)
     ..registerPlugin(Logging())
@@ -70,28 +38,19 @@ void main(List<String> argv) async {
 
   await bot.connect();
 
-  checkYoutube(bot, DateTime.now(), []);
+  checkYoutube(bot, await getVideoIds().toList());
 }
 
-void checkYoutube(INyxxWebsocket bot, DateTime start, List<String> sent) async {
+void checkYoutube(INyxxWebsocket bot, List<String> sent) async {
   log('Searching for new videos/streams...');
-  await yt.channels
-      .getUploads('UCZs3FO5nPvK9VveqJLIvv_w')
-      .asyncMap((v) => getVideo(v.id.value))
-      .where(
-          // unfortunately only works if jana is started at the end of a day
-          // TODO: add everything to sent at the beginning and dont use start
-          (v) => DateTime.tryParse(v['publishDate'])?.isAfter(start) ?? false)
-      .map((v) => v['id'])
-      .where((v) => !sent.contains(v))
-      .forEach((vid) async {
+  await getVideoIds().where((v) => !sent.contains(v)).forEach((vid) async {
     final channel = await bot.fetchChannel<ITextChannel>(news);
     await channel
         .sendMessage(MessageBuilder.content('@everyone https://youtu.be/$vid'));
     sent.add(vid);
   });
   log('Done searching.');
-  Future.delayed(Duration(minutes: 5), () => checkYoutube(bot, start, sent));
+  Future.delayed(Duration(minutes: 5), () => checkYoutube(bot, sent));
 }
 
 extension Str on IMessageAuthor {
